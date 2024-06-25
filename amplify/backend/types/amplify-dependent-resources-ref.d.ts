87044b770755d432a178ca01c8c4e3a14c9a859b
Ismael Rivera <Maelo104@warhorse-m2.lan>
2024-06-25 17:18:15 -0500

    First commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "gettingstarted": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "gettingstarted24a052e9": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "image": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
